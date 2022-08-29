f656f481ec1b6ab9f91619bba8809c6671f970aa
Faithful <faithfulmiracleajah@gmail.com>
2022-08-29 16:10:46 +0100

    deployed website

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { HashRouter as Router } from 'react-router-dom'; 
+import { HashRouter } from 'react-router-dom'; 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
       
   </React.StrictMode>
 )
